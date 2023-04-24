b306bcf6703bde8257fc35d3d8645b3b7dd2f221
dianmusfiroh <dianmusfiroh90@gmail.com>
2023-04-24 22:34:23 +0800

    regis guarf

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            // 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'alamat' => ['required', 'string', 'max:255'],
             'no_hp' => ['required','max:255'],
@@ -71,7 +71,7 @@ class RegisterController extends Controller
     {
         $post = User::create([
             'name' => $data['name'],
-            // 'email' => $data['email'],
+            'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role' =>'masyarakat',
         ]);
